229731946970a3d283bc476720beb4ae39d79857
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-03-22 16:53:20 +0100

    -inizio header-

---
+++ src/components/HeaderDc.vue
@@ -0,0 +1,84 @@
+<template>
+  <header>
+    <div class="container">
+      <img class="img-logo" src="../assets/img/dc-logo.png" alt="logo DC">
+      <ul>
+        <li v-for="(link, index) in links" :key="index"><a href="link.href">{{ link.link }}</a></li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderDc',
+  data () {
+    return {
+      links: [
+        {
+          href: '#',
+          link: 'Characters'
+        },
+        {
+          href: '#',
+          link: 'Comics'
+        },
+        {
+          href: '#',
+          link: 'Movies'
+        },
+        {
+          href: '#',
+          link: 'Tv'
+        },
+        {
+          href: '#',
+          link: 'Games'
+        },
+        {
+          href: '#',
+          link: 'Collectibles'
+        },
+        {
+          href: '#',
+          link: 'Videos'
+        },
+        {
+          href: '#',
+          link: 'Fans'
+        },
+        {
+          href: '#',
+          link: 'News'
+        },
+        {
+          href: '#',
+          link: 'Shop'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    width: 1300px;
+    margin: 20px auto;
+    .img-logo {
+      width: 5%;
+      margin-right: auto;
+    }
+    li {
+    display: inline-block;
+    a {
+      text-decoration: none;
+      color: black;
+      padding: 1rem;
+    }
+  }
+  }
+</style>
