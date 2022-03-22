5579cfe29e8e69275d1f371dc6fe8f664e6835c2
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-03-22 17:44:43 +0100

    -main-

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <HeaderDc />
+    <MainDc />
   </div>
 </template>
 
 <script>
 import HeaderDc from './components/HeaderDc.vue'
+import MainDc from './components/MainDc.vue'
 export default {
   name: 'App',
   components: {
-    HeaderDc
+    HeaderDc,
+    MainDc
   }
 }
 </script>
---
+++ src/components/MainDc.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <div class=" container main-container">
+    <span class="main-text">
+      --> Content goes here
+    </span>
+  </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'MainDc'
+}
+</script>
+
+<style scoped lang="scss">
+@import "../assets/styles/partials/variables";
+main {
+  background-color: black;
+  .main-container {
+    padding: 40px 0;
+    .main-text {
+    color: white;
+    text-transform: uppercase;
+    font-weight: bolder;
+    font-size: 1.1rem;
+  }
+  }
+}
+</style>
