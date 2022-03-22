d9d52faf51d8d387de391c51e50ce7436631931d
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-03-22 19:42:10 +0100

    -footer bottom-

--- src/components/FooterDc.vue
+++ src/components/FooterDc.vue
@@ -2,8 +2,20 @@
   <footer>
     <div class="top-footer">
       <div class="cards" v-for="(element, index) in arrBuy" :key="index">
-            <img class="img-foot" :src="element.img" :alt="element.text">
-           <a :href="element.href"><span class="foot-text">{{ element.text }}</span></a>
+          <img class="img-foot" :src="element.img" :alt="element.text">
+          <a :href="element.href"><span class="foot-text">{{ element.text }}</span></a>
+      </div>
+    </div>
+    <div class="middle-footer">
+      <img class="main-img-foot" src="../assets/img/footer-bg.jpg" alt="">
+    </div>
+    <div class="bottom-footer">
+      <div class="container">
+        <div class="sign-up"><a href="#"><span class="text-sign">Sign-up now!</span></a></div>
+        <div class="social-text">Follow us</div>
+        <a v-for="(element,index) in arrSocial" :key="index" class="social-img" :href="element.href">
+          <img :src="element.img" :alt="element.text">
+        </a>
       </div>
     </div>
   </footer>
@@ -15,6 +27,11 @@ import dcMerch from '../assets/img/buy-comics-merchandise.png'
 import dcShop from '../assets/img/buy-comics-shop-locator.png'
 import dcSubs from '../assets/img/buy-comics-subscriptions.png'
 import dcPower from '../assets/img/buy-dc-power-visa.svg'
+import fb from '../assets/img/footer-facebook.png'
+import twitter from '../assets/img/footer-twitter.png'
+import yt from '../assets/img/footer-youtube.png'
+import pinterest from '../assets/img/footer-pinterest.png'
+import periscope from '../assets/img/footer-periscope.png'
 export default {
   name: 'FooterDc',
   data () {
@@ -44,6 +61,33 @@ export default {
           href: '#',
           img: dcPower,
           text: 'dc power visa'
+        }
+      ],
+      arrSocial: [
+        {
+          href: '#',
+          img: fb,
+          text: 'facebook'
+        },
+        {
+          href: '#',
+          img: twitter,
+          text: 'twitter'
+        },
+        {
+          href: '#',
+          img: yt,
+          text: 'youtube'
+        },
+        {
+          href: '#',
+          img: pinterest,
+          text: 'pinterest'
+        },
+        {
+          href: '#',
+          img: periscope,
+          text: 'periscope'
         }
       ]
     }
@@ -75,6 +119,43 @@ export default {
         color: white;
       }
     }
+  }
+}
+.middle-footer {
+    width: 100%;
+    height: 350px;
+  .main-img-foot {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+.bottom-footer {
+  background-color: #303030;
+  padding: 20px 0;
+  .sign-up {
+    border: 3px solid $blue-color;
+    padding: 10px;
+    a {
+      text-decoration: none;
+    }
+    .text-sign {
+      font-size: 1rem;
+      text-transform: uppercase;
+      font-weight: bolder;
+      color: white;
+    }
+  }
+  .social-text {
+    margin-left: auto;
+    margin-right: 20px;
+    color: $blue-color;
+    text-transform: uppercase;
+    font-weight: bolder;
+    font-size: 1.2rem;
+  }
+  .social-img {
+    margin: 0 10px;
   }
 }
 </style>
