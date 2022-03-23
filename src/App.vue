88136f72bb1d6e57e5dbc64a68fd6d4fe3420173
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-03-23 15:50:31 +0100

    -fix-

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <HeaderDc />
     <MainDc />
-    <CardsDcNovels />
     <FooterDc />
   </div>
 </template>
@@ -10,15 +9,13 @@
 <script>
 import HeaderDc from './components/HeaderDc.vue'
 import MainDc from './components/MainDc.vue'
-import CardsDcNovels from './components/CardsDcNovels.vue'
 import FooterDc from './components/FooterDc.vue'
 export default {
   name: 'App',
   components: {
     HeaderDc,
     MainDc,
-    FooterDc,
-    CardsDcNovels
+    FooterDc
   }
 }
 </script>
